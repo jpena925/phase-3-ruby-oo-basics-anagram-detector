1fb4b7b0c2459bed691dd1b830aabe824fcb3bbb
Jack Pena <penajackc@gmail.com>
2022-02-23 10:10:50 -0700

    commit tree a2fa0273fd8d26c9c5b49c34988e7ce1e8c2e164

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+    def initialize word
+        @word = word
+    end
+
+    def match arr
+        matches_arr =[]
+        arr.each do |word|
+            if word.split('').sort == @word.split('').sort
+                matches_arr << word
+            end
+        end
+        matches_arr
+    end
+end
+
